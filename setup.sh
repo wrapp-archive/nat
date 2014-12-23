95eec71420756e495828ab8421088be68896102a
Eskil Andreen <eskil.andreen@gmail.com>
2014-12-23 15:34:49 +0100

    Add setup.sh for use with userdata scripts

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -ex
+
+INSTANCE_ID=`curl -sL http://169.254.169.254/latest/meta-data/instance-id`
+
+# Enable ip forwarding and masquerading
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+chmod 600 /etc/sysctl.conf
+sysctl -p
+iptables -t nat -A POSTROUTING -s $CIDR_BLOCK -o eth0 -j MASQUERADE
+
+# Disable source destination check
+aws ec2 modify-instance-attribute --region $REGION --instance-id $INSTANCE_ID --no-source-dest-check
+
+# Install the Serf NAT handler
+curl -sL https://raw.githubusercontent.com/wrapp/nat/master/nat-handler.py > /usr/local/bin/nat-handler.py
+chmod a+x /usr/local/bin/nat-handler.py
+
+# Install jq
+apt-get update
+apt-get install -y jq
+
+# Configure Serf to identify itself as NAT and to run the nat handler on member events.
+serfconf=/etc/serf.conf
+tmpconf=/tmp/serf.conf
+cat $serfconf  | jq '.event_handlers += ["member-join,member-leave,member-failed=/usr/local/bin/nat-handler.py"]' > $tmpconf && mv $tmpconf $serfconf
+cat $serfconf  | jq '. += {"tags": {"role": "nat"}}' > $tmpconf && mv $tmpconf $serfconf
+service serf restart
